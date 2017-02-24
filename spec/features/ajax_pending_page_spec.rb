85e0db59f68c3a05be4565644b9c425ac1b56e88
Cody Brubaker <cody.brubaker@gmail.com>
2017-02-24 15:48:34 -0800

    test ajax pending

--- spec/features/ajax_pending_page_spec.rb
+++ spec/features/ajax_pending_page_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe 'test ajax for completing job', js: true do
-  it 'text ajax complete' do
+describe 'test ajax for pending job', js: true do
+  it 'text ajax pending' do
     worker = FactoryGirl.create(:worker)
     login_as(worker, :scope => :worker)
     job = FactoryGirl.create(:job)
     visit job_path(job)
     click_on('Claim')
-    page.find('#job_completed').click
-    click_on('Completed')
+    page.find('#job_pending').click
+    click_on('In Progress')
     expect(page).to have_content 'true'
   end
 end
